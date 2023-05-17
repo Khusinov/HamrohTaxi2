7b9d0c381bcbfa267d421d92ece7f8f7a39894ad
Shaxriyor <shakhriyordev@gmail.com>
2023-05-17 23:40:45 +0500

    login working

--- app/src/main/java/com/khusinov/hamrohtaxi/MainActivity.kt
+++ app/src/main/java/com/khusinov/hamrohtaxi/MainActivity.kt
@@ -17,60 +17,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-//        val navController = findNavController(R.id.nav_host_fragment)
-
-//        navController.setGraph(R.navigation.nav_graph)
-
-
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
 
-//
-//        binding.bottomNavigationView.setOnItemSelectedListener {
-//            when (it.itemId) {
-//                R.id.profileFragment -> {
-//                    navController.navigate(R.id.profileFragment)
-//                    true
-//                }
-//                R.id.searchFragment -> {
-//                    navController.navigate(R.id.searchFragment)
-//                    true
-//                }
-//                R.id.homeFragment -> {
-//                    navController.navigate(R.id.homeFragment)
-//                    true
-//                }
-//                R.id.chatFragment -> {
-//                    navController.navigate(R.id.chatFragment)
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-
-//        navController.addOnDestinationChangedListener { _, destination, _ ->
-//            when (destination.id) {
-//                R.id.homeFragment -> {
-//                    binding.bottomNavigationView.visible(true)
-//                }
-//                R.id.searchFragment -> {
-//                    binding.bottomNavigationView.visible(true)
-//                }
-//                R.id.chatFragment -> {
-//                    binding.bottomNavigationView.visible(true)
-//                }
-//                R.id.profileFragment -> {
-//                    binding.bottomNavigationView.visible(true)
-//                }
-//
-//            }
-//        }
     }
 }
-
-//private fun BottomNavigationView.visible(b: Boolean) {
-//    this.visibility = if (b) View.VISIBLE else View.GONE
-//}
