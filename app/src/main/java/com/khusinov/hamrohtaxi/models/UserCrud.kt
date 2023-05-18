8a7ae648b3a376badeadd93a0df47a3a611180e7
Shaxriyor <shakhriyordev@gmail.com>
2023-05-18 23:10:33 +0500

    user crud

---
+++ app/src/main/java/com/khusinov/hamrohtaxi/models/UserCrud.kt
@@ -0,0 +1,8 @@
+package com.khusinov.hamrohtaxi.models
+
+data class UserCrud(
+    var name:String? = null,
+    var phone_number: String? = null,
+    var password: String? = null,
+    var user_role:String? = null
+)
