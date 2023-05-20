c723e7affdcda2c1bfbcea7d8a3930795171fcd0
Shaxriyor <shakhriyordev@gmail.com>
2023-05-20 17:02:56 +0500

    home adapter working

--- app/src/main/java/com/khusinov/hamrohtaxi/HomeFragment.kt
+++ app/src/main/java/com/khusinov/hamrohtaxi/HomeFragment.kt
@@ -1,15 +1,22 @@
 package com.khusinov.hamrohtaxi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.khusinov.hamrohtaxi.databinding.FragmentHomeBinding
+import com.khusinov.hamrohtaxi.models.Post
+import com.khusinov.hamrohtaxi.network.Common
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding by viewBinding { FragmentHomeBinding.bind(it) }
+    private val TAG = "HomeFragment"
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUI()
@@ -18,8 +25,74 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private fun setupUI() {
         binding.apply {
 
+            val postList = ArrayList<Post>()
+            Common.retrofitServices.getAllPosts().enqueue(object : Callback<List<Post>> {
+                override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+
+                    Log.d(TAG, "onResponse: posts ${response.isSuccessful}")
+                    Log.d(TAG, "onResponse: posts ${response.body()}")
+
+                    if (response.isSuccessful) {
+
+                        val list = response.body()
+                        if (list != null) {
+                            for (i in list) {
+                                val user = i.user
+                                val from = i.from_location
+                                val to = i.to_location
+                                val goTime = i.go_time
+                                val postedTime = i.posted_time
+                                val postId = i.id
+                                val addition = i.addition
+                                val count = i.count
+                                val role = i.user_role
+                                val status = i.status
+
+                                val post = Post(
+                                    addition,
+                                    count,
+                                    from,
+                                    goTime,
+                                    postId,
+                                    postedTime,
+                                    status,
+                                    to,
+                                    user,
+                                    role
+                                )
+
+                                postList.add(post)
+                            }
+                            callAdapter(postList)
+                        }
+
+
+                    }
+
+                }
+
+                override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+
+                    Log.d(TAG, "onFailure: ${t.message}")
+                }
+
+            })
+
 
         }
 
+    }
+
+    private fun callAdapter(postList: java.util.ArrayList<Post>) {
+
+        binding.apply {
+
+
+            val adapter = PostAdapter()
+            var recyclerView = rv
+
+            rv.adapter = adapter
+            adapter.submitList(postList)
+        }
     }
 }
---
+++ app/src/main/java/com/khusinov/hamrohtaxi/PostAdapter.kt
@@ -0,0 +1,83 @@
+package com.khusinov.hamrohtaxi
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.khusinov.hamrohtaxi.databinding.ItemPostBinding
+import com.khusinov.hamrohtaxi.models.Post
+
+
+class PostAdapter() : RecyclerView.Adapter<PostAdapter.PartOneViewHolder>() {
+
+    private val dif = AsyncListDiffer(this, ITEM_DIFF)
+
+    //    var onClick: ((Flower) -> Unit)? = null
+//    var onClick2: ((Flower) -> Unit)? = null
+//    var onClick3: ((Flower) -> Unit)? = null
+    private val TAG = "PostAdapter"
+
+    inner class PartOneViewHolder(private val binding: ItemPostBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind() {
+
+            val postCurrent = dif.currentList[adapterPosition]
+
+            binding.apply {
+
+                var locationFrom1 = postCurrent.from_location
+                var index = locationFrom1.indexOf("-")
+
+                var locationFrom2 = locationFrom1.substring(index + 1)
+                Log.d(TAG, "bind: location $locationFrom2")
+
+                var locationTo1 = postCurrent.to_location
+                var index2 = locationTo1.indexOf("-")
+                var locationTo2 = locationTo1.substring(index2+1)
+                Log.d(TAG, "bind: location $locationTo2")
+
+
+                name.text = postCurrent.user.name
+                locationFrom.text = locationFrom2
+                locationTo.text =locationTo2
+                time.text = postCurrent.go_time
+                postedTime.text = postCurrent.posted_time
+                personCount.text = postCurrent.count.toString()
+
+
+                binding.root.setOnClickListener {
+
+                }
+
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PartOneViewHolder =
+        PartOneViewHolder(
+            ItemPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+
+    override fun getItemCount(): Int = dif.currentList.size
+
+    override fun onBindViewHolder(holder: PartOneViewHolder, position: Int) = holder.bind()
+
+    fun submitList(list: List<Post>) {
+        dif.submitList(list)
+    }
+
+    companion object {
+        private val ITEM_DIFF = object : DiffUtil.ItemCallback<Post>() {
+            override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean =
+                oldItem.user == newItem.user
+
+            @SuppressLint("DiffUtilEquals")
+            override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean =
+                oldItem == newItem
+        }
+    }
+
+}
